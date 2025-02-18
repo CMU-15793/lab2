be14c54588a5adf976045ed9ca3086502d69ebf7
Wenting Zheng <wzheng13@gmail.com>
2025-02-18 13:57:26 -0500

    Added test.cc

---
+++ src/test.cc
@@ -0,0 +1,15 @@
+#include "openfhe.h"
+
+using namespace lbcrypto;
+
+int main() {
+
+  /** TODO: read in sample public keys and private keys **/
+
+  /** TODO: generate one rotation key **/
+
+  /** TODO: implement dot product of two ciphertexts **/
+
+  return 0;
+
+}
